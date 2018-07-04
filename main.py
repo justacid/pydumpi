7c18ff248cb3a1f2940649b643363891c5b66f1f
Tobias Schwackenhofer <tobiasschw@gmail.com>
2018-07-04 15:19:04 +0200

    Add trace filepath utilities

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+from dumpi.util import trace_files_from_dir
 from dumpi.undumpi import DumpiTrace
-from dumpi.constants import DataType
 import matplotlib.pyplot as plt
 from pathlib import Path
-from os import listdir
 import sys
 
 
@@ -20,41 +19,27 @@ class MessageTrace(DumpiTrace):
 
     
 if __name__ == "__main__":
-    
+
     folder = sys.argv[1]
-    files = listdir(folder);
-    # don't count the meta file
-    file_count = len(files)-1;
+    if not Path(folder).is_dir():
+        raise ValueError(f"{folder} is not a folder.")
 
     message_count = 0
     message_sizes = []
 
-    for i, file_name in enumerate(files):
-        binary_dump = Path(folder) / file_name
+    for i, path in enumerate(trace_files_from_dir(folder)):
+        with MessageTrace(path) as trace:
+            if i == 0:
+                trace.print_header()
+                trace.print_sizes()
 
-        # get number of processes from meta file for sanity check
-        if file_name.rfind("meta") != -1:
-            num_procs = None
-            with open(binary_dump, "r") as meta_file:
-                for line in meta_file:
-                    if line.startswith("numprocs"):
-                        num_procs = int(line.split("=")[1])
-            assert num_procs == file_count
-            continue
-
-        # analyze binary dumps
-        with MessageTrace(binary_dump) as trace:
-            trace.print_header()
             trace.print_footer()
-            trace.print_sizes()
-
             trace.read_stream()
             message_count += trace.message_count
             message_sizes.extend(trace.message_sizes)
 
             print(f"{trace.message_count} messages parsed.")
-            print(f"Done with file '{file_name}'...\n")
-
+            print(f"Done with file '{path}'...\n")
 
     print(f"Total messages sent: {message_count}")
     # convert sizes to kbyte

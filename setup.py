97b4c16008ab4572ac877e259f4ee2f6839405db
Tobias Schwackenhofer <tobiasschw@gmail.com>
2018-07-09 21:08:18 +0200

    Add setup.py
    Also removed files that are not relevant to the dumpi
    bindings package itself. They are moved to a separate
    repository.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pydumpi",
+    version="0.0.1",
+    author="Tobias Schwackenhofer",
+    author_email="tobiasschw@gmail.com",
+    description="Python bindings for the sst-dumpi trace format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/justacid/python-dumpi",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    )
+)
